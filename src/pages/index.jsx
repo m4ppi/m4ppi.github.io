889b7eab524c16a335771a0d929b8ce658160230
m4tsumoto <macchallatte@gmail.com>
2018-07-18 08:08:07 +0900

    upd index.html

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -23,6 +23,23 @@ class IndexRoute extends React.Component {
           <div className="content__inner">
             {/*記事一覧*/}
             {/*{items}*/}
+            <div className="page">
+              <h1 className="page__title">About me</h1>
+              <p>m4ppi(まっぴ)</p>
+              <p>Tokyo</p>
+              <p>1994/09/09</p>
+              <p>2018年10月からエンジニアとして働き始めました。</p>
+              <p>業務では、最近はphpを書いてます。
+                以前はjavaをメインに書いていていましたが、最近はほぼ触ってないです。
+                node.js(Express)でのプロジェクトも携わっていましたが、非同期処理あたりがまだ苦手です。</p>
+              <p>最近はGo言語にハマっていて、簡単なツールはGo言語で書いています。</p>
+              <h2>Contact</h2>
+              <ul>
+                <li><a href="https://www.twitter.com/m4ppi">twitter</a></li>
+                <li><a href="https://www.github.com/m4ppi">GitHub</a></li>
+              </ul>
+              <p><img src="https://source.unsplash.com/random/800x600" alt="unsplash random" /></p>
+            </div>
           </div>
         </div>
       </div>
